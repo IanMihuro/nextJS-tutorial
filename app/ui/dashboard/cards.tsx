e83dd840b46f8cff637fef0f2fc480fd7fa20bb2
ian mihuro <ianmihuro@ians-MacBook-Pro-3.local>
2024-07-02 21:05:42 +0300

    chapter 9: suspense loading complete

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -12,18 +12,15 @@ const iconMap = {
   pending: ClockIcon,
   invoices: InboxIcon,
 };
+import { fetchCardData } from '@/app/lib/data';
 
-export default async function CardWrapper({
-  totalPaidInvoices,
-  totalPendingInvoices,
-  numberOfInvoices,
-  numberOfCustomers,
-}: {
-  totalPaidInvoices: number;
-  totalPendingInvoices: number;
-  numberOfInvoices: number;
-  numberOfCustomers: number;
-}) {
+export default async function CardWrapper() {
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
